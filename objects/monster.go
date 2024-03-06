98f47cb20501e2e4954055dfbfa3165601690d79
Vince Kyle <vincekyleparcon@gmail.com>
2024-03-06 09:24:03 +0800

    changed self -> s to remove generic terms

--- objects/monster.go
+++ objects/monster.go
@@ -1,10 +1,12 @@
 package objects
 
 type Monster struct {
-    Name string
-    Life int
-    Atk  int
-    Def  int
+	Name string
+	Life int
+	Atk  int
+	Def  int
 }
 
-
+func (mons *Monster) AttackPlayer(target *Player) {
+	target.Life = target.Life + target.Def - mons.Atk
+}
