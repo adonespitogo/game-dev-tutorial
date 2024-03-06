9587012ecaabaa36c2cf8f5df2046c0b995635e4
Vince Kyle <vincekyleparcon@gmail.com>
2024-03-06 13:47:10 +0800

    cleanup and Name change

--- objects/scene.go
+++ objects/scene.go
@@ -17,8 +17,8 @@ func (scene *Scene) Start() {
 
 	scene.Mage = &Player{Name: "Kyle", Life: 100, Atk: 10, Def: 5, Magic: 25}
 	scene.Warrior = &Player{Name: "Carlo", Life: 150, Atk: 20, Def: 10, Magic: 10}
-	scene.Goblin = &Monster{Name: "Gobs", Life: 50, Atk: 15, Def: 5, Magic: 5}
-	scene.Slime = &Monster{Name: "Gobs", Life: 15, Atk: 10, Def: 2, Magic: 3}
+	scene.Goblin = &Monster{Name: "Goblin King", Life: 50, Atk: 15, Def: 5, Magic: 5}
+	scene.Slime = &Monster{Name: "Mega Slime", Life: 15, Atk: 10, Def: 2, Magic: 3}
 
 	fmt.Printf("Monster life: %d\n", scene.Goblin.Life)
 
@@ -34,7 +34,7 @@ func (scene *Scene) Start() {
 }
 
 func (scene *Scene) End() {
-	fmt.Printf("Game over! Mage Life: %d", scene.Mage.Life)
+	fmt.Printf("Game over! Mage Life: %d\n", scene.Mage.Life)
 }
 
 func (scene *Scene) Battle(monster *Monster) {
@@ -47,11 +47,11 @@ func (scene *Scene) Battle(monster *Monster) {
 	for !(scene.Mage.IsDead() && scene.Warrior.IsDead() || monster.IsDead()) {
 
 		if x%2 == 0 { // 1st Turn
-			fmt.Print("2: ")
+			fmt.Print("1: ")
 
 			if !scene.Warrior.IsDead() {
 				scene.Warrior.AttackMonster(monster)
-				fmt.Printf("%s attacked %s", scene.Warrior.Name, monster.Name)
+				fmt.Printf("%s attacked %s\n", scene.Warrior.Name, monster.Name)
 			} else {
 				fmt.Println("Player Dead - Skip Turn")
 			}
@@ -59,11 +59,11 @@ func (scene *Scene) Battle(monster *Monster) {
 			x++
 
 		} else if x > y { // 2nd Turn
-			fmt.Print("1: ")
+			fmt.Print("2: ")
 
 			if !scene.Mage.IsDead() {
 				scene.Mage.AttackMonster(monster)
-				fmt.Printf("%s attacked %s", scene.Mage.Name, monster.Name)
+				fmt.Printf("%s attacked %s\n", scene.Mage.Name, monster.Name)
 			} else {
 				fmt.Println("Player Dead - Skip Turn")
 			}
@@ -75,7 +75,7 @@ func (scene *Scene) Battle(monster *Monster) {
 
 			monster.AttackPlayer(scene.Mage)
 			monster.AttackPlayer(scene.Warrior)
-			fmt.Printf("%s launched an attack", monster.Name)
+			fmt.Printf("%s launched an attack\n", monster.Name)
 
 			x++
 			y++
