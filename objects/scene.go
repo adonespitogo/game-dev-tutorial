d54550732d75fcc5d66968cd0b473ee7aa4f05c4
Vince Kyle <vincekyleparcon@gmail.com>
2024-03-06 10:59:55 +0800

    Created Battle() Function

--- objects/scene.go
+++ objects/scene.go
@@ -9,6 +9,7 @@ func NewScene() *Scene {
 type Scene struct {
 	Mage    *Player
 	Warrior *Player
+	Monster *Monster
 }
 
 func (scene *Scene) Start() {
@@ -32,4 +33,53 @@ func (scene *Scene) Start() {
 
 func (scene *Scene) End() {
 	fmt.Printf("Game over! Mage Life: %d", scene.Mage.Life)
+}
+
+func Battle(p1 *Player, p2 *Player, p3 *Monster) {
+
+	x := 0
+	y := 0
+
+	fmt.Println("Battle Start")
+
+	for !(p1.IsDead() && p2.IsDead() || p3.IsDead()) {
+
+		if x > y { // 2nd Turn
+			fmt.Print("2: ")
+
+			if !p2.IsDead() {
+				p2.AttackMonster(p3)
+				fmt.Printf("%s attacked %s", p2.Name, p3.Name)
+			} else {
+				fmt.Println("Player Dead - Skip Turn")
+			}
+
+			y++
+
+		} else if x%2 == 0 { // 1st Turn
+			fmt.Print("1: ")
+
+			if !p1.IsDead() {
+				p1.AttackMonster(p3)
+				fmt.Printf("%s attacked %s", p1.Name, p3.Name)
+			} else {
+				fmt.Println("Player Dead - Skip Turn")
+			}
+
+			x++
+
+		} else { // 3rd turn
+			fmt.Print("3: ")
+			p3.AttackPlayer(p1)
+			p3.AttackPlayer(p2)
+			fmt.Printf("%s launched an attack", p3.Name)
+			x++
+			y++
+
+		}
+
+	}
+
+	fmt.Println("Battle End")
+
 }
