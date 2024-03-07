f96383c41b4bbf25f4749da5e5cbd66be1230158
Vince Kyle <vincekyleparcon@gmail.com>
2024-03-07 11:34:13 +0800

    Battle Method changes

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
-import "github.com/adonespitogo/game-dev-tutorial/objects"
+import (
+	"fmt"
+
+	"github.com/adonespitogo/game-dev-tutorial/objects"
+)
 
 func main() {
 	scene := objects.NewScene()
 	scene.Start()
-	scene.Battle(scene.Goblin)
+
 	scene.Battle(scene.Slime)
+
+	if scene.AllPlayerDead() {
+		fmt.Println("GAME OVER, YOU LOSE")
+	} else {
+		scene.Battle(scene.Goblin)
+	}
+
 }
--- objects/scene.go
+++ objects/scene.go
@@ -18,37 +18,19 @@ func (scene *Scene) Start() {
 
 	scene.Mage = &Player{Name: "Kyle", Life: 100, Atk: 10, Def: 5, Magic: 25}
 	scene.Warrior = &Player{Name: "Carlo", Life: 150, Atk: 20, Def: 10, Magic: 10}
-	scene.Goblin = &Monster{Name: "Goblin King", Life: 50, Atk: 15, Def: 5, Magic: 5}
+	scene.Goblin = &Monster{Name: "Goblin King", Life: 50, Atk: 1500, Def: 500, Magic: 5}
 	scene.Slime = &Monster{Name: "Mega Slime", Life: 15, Atk: 10, Def: 2, Magic: 3}
 	scene.Archer = &Player{Name: "Marc", Life: 100, Atk: 20, Def: 2, Magic: 1}
 
-	fmt.Printf("Monster life: %d\n", scene.Goblin.Life)
-
-	scene.Warrior.AttackMonster(scene.Goblin)
-
-	if scene.Mage.IsDead() {
-		fmt.Println("Mage is dead")
-	}
-
-	fmt.Printf("Goblin life after attack: %d\n", scene.Goblin.Life)
-
-	scene.End()
-}
-
-func (scene *Scene) End() {
-	fmt.Printf("Game over! Mage Life: %d\n", scene.Mage.Life)
 }
 
 func (scene *Scene) Battle(monster *Monster) {
 
-	x := 0
-	y := 0
-
 	fmt.Println("Battle Start")
 
-	for !(scene.Mage.IsDead() && scene.Warrior.IsDead() || monster.IsDead()) {
+	for x := 0; !(scene.AllPlayerDead() || monster.IsDead()); x++ {
 
-		if x%2 == 0 { // 1st Turn
+		if x == 0 { // 1st Turn
 
 			fmt.Print("1: ")
 			if !scene.Warrior.IsDead() {
@@ -56,9 +38,8 @@ func (scene *Scene) Battle(monster *Monster) {
 			} else {
 				fmt.Println("Player Dead - Skip turn")
 			}
-			x++
 
-		} else if x > y { // 2nd Turn
+		} else if x == 1 { // 2nd Turn
 
 			fmt.Print("2: ")
 			if !scene.Mage.IsDead() {
@@ -66,21 +47,40 @@ func (scene *Scene) Battle(monster *Monster) {
 			} else {
 				fmt.Println("Player Dead - Skip turn")
 			}
-			y++
 
-		} else { // 3rd turn
+		} else if x == 2 { // 2nd Turn
 
 			fmt.Print("3: ")
+			if !scene.Archer.IsDead() {
+				scene.Archer.AttackMonster(monster)
+			} else {
+				fmt.Println("Player Dead - Skip turn")
+			}
+
+		} else if x == 3 { // 3rd turn
+
+			fmt.Print("4: ")
 			monster.AttackPlayer(scene.Mage)
 			monster.AttackPlayer(scene.Warrior)
+			monster.AttackPlayer(scene.Archer)
 			fmt.Printf("%s launched an attack\n", monster.Name)
-			x++
-			y++
 
+		} else {
+			x = -1
 		}
-
 	}
 	//Dodongcaloy was here
 	fmt.Println("Battle End")
+}
 
+func (scene *Scene) AllPlayerDead() bool {
+	return scene.Mage.IsDead() && scene.Warrior.IsDead() && scene.Archer.IsDead()
+}
+
+func (scene *Scene) AllMonsterDead() bool {
+	return scene.Goblin.IsDead() && scene.Slime.IsDead()
+}
+
+func (scene *Scene) End() {
+	fmt.Printf("Game over! Mage Life: %d\n", scene.Mage.Life)
 }
